0886ecea5ec8403f97fec2d409b2f75cc4af6f5b
Harry Nicholls <harry.m.nicholls@gmail.com>
2019-12-20 14:00:32 -0500

    Adds dialog backdrop

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -6,9 +6,15 @@ interface DialogProps {
 
 const Dialog = ({ children }: DialogProps) => {
   return (
-    <dialog className="absolute left-0 right-0 center bg-white pa3 ba bw2 mw6 flex flex-column justify-center top-50" open={true}>
-      {children}
-    </dialog>
+    <>
+      <dialog
+        className="absolute z-2 left-0 right-0 center bg-white pa3 ba bw2 mw6 flex flex-column justify-center top-50"
+        open={true}
+      >
+        {children}
+      </dialog>
+      <div className="fixed top-0 right-0 left-0 bottom-0 bg-black-40" />
+    </>
   );
 };
 
